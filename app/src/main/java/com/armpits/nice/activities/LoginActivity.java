3a962edc1dc666d023087efe3ec60e8ea184d178
Cillo98 <thomas.martin.cilloni@gmail.com>
2019-11-10 23:38:34 +0800

    Added Login Activity

---
+++ app/src/main/java/com/armpits/nice/activities/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.armpits.nice.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.armpits.nice.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
