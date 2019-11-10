b19505536c6c415e49905f2f33e76d6ca38f2d24
Cillo98 <thomas.martin.cilloni@gmail.com>
2019-11-10 23:28:22 +0800

    Created Entry Point Activity to the application

---
+++ app/src/main/java/com/armpits/nice/activities/EntryPointActivity.java
@@ -0,0 +1,27 @@
+package com.armpits.nice.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.armpits.nice.utils.Const;
+import com.armpits.nice.utils.SharedPreferencesManager;
+
+public class EntryPointActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent startAppIntent;
+
+        // if the user is authenticated, go to the main activity, login otherwise
+        if (SharedPreferencesManager.getSharedPreference(Const.SP_HAS_LOGGED_IN, this) != null)
+            startAppIntent = new Intent(this, MainActivity.class);
+        else
+            startAppIntent = new Intent(this, LoginActivity.class);
+
+        startActivity(startAppIntent);
+        finish();
+    }
+}
