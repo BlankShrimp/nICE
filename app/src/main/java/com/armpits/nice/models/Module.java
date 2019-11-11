6c8f66ed486ca50145e3c257f966f7dcce483b8b
Cillo98 <thomas.martin.cilloni@gmail.com>
2019-11-11 16:42:44 +0800

    Added fields to module

--- app/src/main/java/com/armpits/nice/models/Module.java
+++ app/src/main/java/com/armpits/nice/models/Module.java
@@ -5,19 +5,30 @@ import java.util.List;
 
 public class Module {
     public final String title;
+    public final String code;
     public final Date lastUpdate;
     public final boolean enableNotifications;
+    public final boolean enableDownloads;
     public final boolean addDDLsToCalendar;
     public final List<Material> materials;
     public final List<Deadline> deadlines;
 
-    public Module(String title, Date lastUpdate, boolean enableNotifications,
-                  boolean addDDLsToCalendar, List<Material> materials, List<Deadline> deadlines) {
+    public Module(String title, String code, Date lastUpdate,
+                  boolean enableNotifications, boolean addDDLsToCalendar, boolean enableDownloads,
+                  List<Material> materials, List<Deadline> deadlines) {
         this.title = title;
+        this.code = code;
         this.lastUpdate = lastUpdate;
         this.enableNotifications = enableNotifications;
+        this.enableDownloads = enableDownloads;
         this.addDDLsToCalendar = addDDLsToCalendar;
         this.materials = materials;
         this.deadlines = deadlines;
+    }
+
+    public Module(String title, String code, Date lastUpdate,
+                  boolean enableNotifications, boolean addDDLsToCalendar, boolean enableDownloads) {
+        this (title, code, lastUpdate, enableNotifications, addDDLsToCalendar, enableDownloads,
+                null, null);
     }
 }
