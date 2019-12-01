c624952bdba5f3c0ade7c8bc1d429682596647cf
Cillo98 <thomas.martin.cilloni@gmail.com>
2019-12-01 18:03:48 +0800

    Added login functionality to the login activity

--- app/src/main/java/com/armpits/nice/activities/LoginActivity.java
+++ app/src/main/java/com/armpits/nice/activities/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.armpits.nice.activities;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.armpits.nice.R;
+import com.armpits.nice.networking.Networking;
 import com.armpits.nice.utils.Const;
 import com.armpits.nice.utils.SharedPreferencesManager;
 
@@ -28,13 +31,36 @@ public class LoginActivity extends AppCompatActivity {
                 String username = txtUsername.getText().toString();
                 String password = txtPassword.getText().toString();
 
+                // check if username and password work
+                boolean credentialsAreValid = tryToLogin(username, password);
+                if (!credentialsAreValid) return;
+
                 SharedPreferencesManager.set(Const.SP_USERNAME, username, LoginActivity.this);
                 SharedPreferencesManager.set(Const.SP_PASSWORD, password, LoginActivity.this);
                 SharedPreferencesManager.set(Const.SP_LOGGED_IN,
                         Const.SP_LOGGED_IN_TRUE, LoginActivity.this);
-
                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
             }
         });
+    }
+
+    private boolean tryToLogin(String username, String password) {
+        Networking client = new Networking();
+        String token = client.fetchToken();
+
+        if (token == null) {
+            Toast.makeText(LoginActivity.this, "Error connecting to ICE", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        String[] response = client.login(token, username, password);
+        String status = response[0];
+
+        if (status.equals("Failed")) {
+            Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_LONG).show();
+            return false;
+        }
+
+        return true;
     }
 }
