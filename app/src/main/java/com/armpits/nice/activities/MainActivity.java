30f03c768992b295b2fa06f50d0d4e73ef351d25
Cillo98 <thomas.martin.cilloni@gmail.com>
2019-12-02 17:05:18 +0800

    Moved database I/O from activity (delegation) to the NiceDatabase class

--- app/src/main/java/com/armpits/nice/activities/MainActivity.java
+++ app/src/main/java/com/armpits/nice/activities/MainActivity.java
@@ -1,16 +1,10 @@
 package com.armpits.nice.activities;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 
 import com.armpits.nice.R;
-import com.armpits.nice.db.DbHolder;
 import com.armpits.nice.db.NiceDatabase;
-import com.armpits.nice.models.Deadline;
-import com.armpits.nice.models.Material;
-import com.armpits.nice.models.Module;
-import com.armpits.nice.networking.Parser;
-import com.armpits.nice.utils.Const;
-import com.armpits.nice.utils.SharedPreferencesManager;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,22 +12,17 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.room.Room;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
-public class MainActivity extends AppCompatActivity implements DbHolder {
-    private NiceDatabase db;
-    private String username;
-    private String password;
+    public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
+
+        // allow internet
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().permitAll().build());
 
         // Passing each menu ID as a set of Ids because each menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
@@ -42,38 +31,7 @@ public class MainActivity extends AppCompatActivity implements DbHolder {
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 
-        // instantiate the database and get the credentials
-        db = Room.databaseBuilder(getApplicationContext(), NiceDatabase.class, "nice_db").build();
-        username = SharedPreferencesManager.get(Const.SP_USERNAME, this);
-        password = SharedPreferencesManager.get(Const.SP_PASSWORD, this);
-    }
-
-    @Override
-    public List<Module> getAllModules() {
-        // get modules from DB, but if these are empty then get them from online and save them to DB
-        List<Module> modules = db.moduleDao().getAll().getValue();
-
-        if (modules == null || modules.isEmpty()) {
-            modules = new ArrayList<>();
-            List<String[]> modulesOnline = Parser.getCoursesList(username, password);
-
-            for (String[] moduleInfo : modulesOnline)
-                modules.add(new Module(moduleInfo[0], moduleInfo[1], new Date(),
-                false, false, false));
-
-            db.moduleDao().insertAll(modules.toArray(new Module[]{}));
-        }
-
-        return modules;
-    }
-
-    @Override
-    public List<Deadline> getAllMDeadlines() {
-        return db.deadlineDao().getAll().getValue();
-    }
-
-    @Override
-    public List<Material> getAllMaterial() {
-        return db.materialDao().getAll().getValue();
+        // instantiate the database
+        NiceDatabase db = NiceDatabase.getDatabase(this);
     }
 }
--- app/src/main/java/com/armpits/nice/db/NiceDatabase.java
+++ app/src/main/java/com/armpits/nice/db/NiceDatabase.java
@@ -1,12 +1,19 @@
 package com.armpits.nice.db;
 
+import android.content.Context;
+
 import com.armpits.nice.models.Deadline;
 import com.armpits.nice.models.Material;
 import com.armpits.nice.models.Module;
 
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.TypeConverters;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Database(entities = {Deadline.class, Module.class, Material.class}, version = 1, exportSchema = false)
 @TypeConverters({DateConverter.class})
@@ -15,4 +22,38 @@ public abstract class NiceDatabase extends RoomDatabase {
     public abstract DaoDeadline deadlineDao();
     public abstract DaoModule   moduleDao();
     public abstract DaoMaterial materialDao();
+
+    private static volatile NiceDatabase INSTANCE;
+    private static final int N_THREADS = 4;
+    static final ExecutorService dbWriter = Executors.newFixedThreadPool(N_THREADS);
+
+    public static NiceDatabase getDatabase(final Context context) {
+        if (INSTANCE == null)
+            synchronized (NiceDatabase.class) {
+                if (INSTANCE == null)
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            NiceDatabase.class, "nice_database").build();
+            }
+        return INSTANCE;
+    }
+
+    public static List<Deadline> getAllDeadlines() {
+        return INSTANCE.deadlineDao().getAll().getValue();
+    }
+    public static List<Material> getAllMaterials() {
+        return INSTANCE.materialDao().getAll().getValue();
+    }
+    public static List<Module> getAllModules() {
+        return INSTANCE.moduleDao().getAll().getValue();
+    }
+
+    public static void insert(Deadline deadline) {
+        dbWriter.execute(() -> INSTANCE.deadlineDao().insertAll(deadline));
+    }
+    public static void insert(Material material) {
+        dbWriter.execute(() -> INSTANCE.materialDao().insertAll(material));
+    }
+    public static void insert(Module module) {
+        dbWriter.execute(() -> INSTANCE.moduleDao().insertAll(module));
+    }
 }
