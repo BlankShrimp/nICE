8f7f61aff60bf2c08e87e569ca92fc34215fe9f4
BlankShrimp <vanishing.19@gmail.com>
2019-12-12 03:33:03 +0800

    Add downloading feature.

--- app/src/main/java/com/armpits/nice/networking/Networking.java
+++ app/src/main/java/com/armpits/nice/networking/Networking.java
@@ -1,8 +1,18 @@
 package com.armpits.nice.networking;
 
+import android.os.Environment;
+
+import androidx.annotation.NonNull;
+
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -162,5 +172,75 @@ public class Networking {
             System.out.println(e);
         }
         return result;
+    }
+
+    public void download(String module, String parent, String url, OnDownloadListener listener) {
+        Request request = new Request.Builder().url(url).build();
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                listener.onDownloadFailed();
+            }
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                InputStream is = null;
+                byte[] buf = new byte[2048];
+                int len = 0;
+                FileOutputStream fos = null;
+                String savePath = isExistDir("Documents/nICE/" + module + "/" +parent);
+                try {
+                    is = response.body().byteStream();
+                    long total = response.body().contentLength();
+                    File file = new File(savePath, getNameFromUrl(URLDecoder.decode(url, "UTF-8")));
+                    fos = new FileOutputStream(file);
+                    long sum = 0;
+                    while ((len = is.read(buf)) != -1) {
+                        fos.write(buf, 0, len);
+                        sum += len;
+                        int progress = (int) (sum * 1.0f / total * 100);
+                        listener.onDownloading(progress);
+                    }
+                    fos.flush();
+                    listener.onDownloadSuccess();
+                } catch (Exception e) {
+                    System.out.println("--ERROR");
+                    System.out.println(e);
+                    listener.onDownloadFailed();
+                } finally {
+                    try {
+                        if (is != null)
+                            is.close();
+                    } catch (IOException e) {
+                    }
+                    try {
+                        if (fos != null)
+                            fos.close();
+                    } catch (IOException e) {
+                    }
+                }
+            }
+        });
+    }
+
+    private String isExistDir(String saveDir) throws IOException {
+        File downloadFile = new File(Environment.getExternalStorageDirectory(), saveDir);
+        if (!downloadFile.mkdirs()) {
+            downloadFile.createNewFile();
+        }
+        String savePath = downloadFile.getAbsolutePath();
+        return savePath;
+    }
+
+    @NonNull
+    private String getNameFromUrl(String url) {
+        return url.substring(url.lastIndexOf("/") + 1);
+    }
+
+    public interface OnDownloadListener {
+        void onDownloadSuccess();
+
+        void onDownloading(int progress);
+
+        void onDownloadFailed();
     }
 }
