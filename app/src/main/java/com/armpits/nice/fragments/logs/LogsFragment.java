9e8f118a2e6212447bac3a57bd66bd489bf00e7f
Cillo98 <thomas.martin.cilloni@gmail.com>
2019-12-16 02:35:45 +0800

    Added Logs fragment

--- app/src/main/java/com/armpits/nice/fragments/logs/LogsFragment.java
+++ app/src/main/java/com/armpits/nice/fragments/logs/LogsFragment.java
@@ -4,32 +4,69 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.armpits.nice.R;
+import com.armpits.nice.db.NiceDatabase;
+import com.armpits.nice.models.Log;
+import com.armpits.nice.models.Module;
+import com.armpits.nice.ui.LogsAdapter;
+import com.armpits.nice.ui.ModulesAdapter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class LogsFragment extends Fragment {
-
-    private LogsViewModel logsViewModel;
+    private View mContainer;
+    private RecyclerView recyclerView;
+    private LogsAdapter adapter;
+    private LiveData<List<Log>> logsLiveData;
+    private List<Log> logs;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        logsViewModel =
-                ViewModelProviders.of(this).get(LogsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_logs, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        logsViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
+        mContainer = inflater.inflate(R.layout.fragment_logs, container, false);
+        recyclerView = mContainer.findViewById(R.id.recycler_view_logs);
+
+        logsLiveData = NiceDatabase.getAllLogs();
+        logs = new ArrayList<>();
+        adapter = new LogsAdapter(mContainer.getContext(), logs);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(mContainer.getContext()));
+        recyclerView.setAdapter(adapter);
+
+        logsLiveData.observe(this, updatedLogs -> {
+            logs.clear();
+            logs.addAll(updatedLogs);
+            adapter.notifyDataSetChanged();
         });
-        return root;
+
+        return mContainer;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // add some logs if empty
+        new Thread(() -> {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (logs.isEmpty())
+                for (int i=0; i<100; i++)
+                    NiceDatabase.insert(new Log(new Date(System.currentTimeMillis()-i*100000),
+                            "log number "+i+": this is a sample message. For displaying data in lists, we use what is called a RecyclerView. It uses an Adapter to control the display of each item in the list by asking you to first create a view for each position, and then define a way to update these views for the data at each position."
+                    .substring(0, (int) (200*Math.random()))));
+        }).start();
     }
 }
