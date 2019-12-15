95ddaa16c5be0330c687dca74edca9e5f43aae4a
BlankShrimp <vanishing.19@gmail.com>
2019-12-15 22:26:19 +0800

    Update attributes in material.

--- app/src/main/java/com/armpits/nice/models/Material.java
+++ app/src/main/java/com/armpits/nice/models/Material.java
@@ -10,13 +10,20 @@ public class Material {
     @NonNull public final String moduleCode;
     @NonNull public final String filename;
     @NonNull public final String parentPath;
+    @NonNull public final String url;
+    @NonNull public final String description;
+    public final String moduleTitle;
     public Date dateDownloaded;
     public boolean shouldDownload;
 
-    public Material( String moduleCode, String filename, String parentPath, Date dateDownloaded, boolean shouldDownload) {
+    public Material(String moduleTitle, String moduleCode, String filename, String parentPath,
+                    String url, String description, Date dateDownloaded, boolean shouldDownload) {
+        this.moduleTitle = moduleTitle;
         this.moduleCode = moduleCode;
         this.filename = filename;
         this.parentPath = parentPath;
+        this.url = url;
+        this.description = description;
         this.dateDownloaded = dateDownloaded;
         this.shouldDownload = shouldDownload;
     }
