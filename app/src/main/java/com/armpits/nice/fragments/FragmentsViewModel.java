70f1da3970aa1dacdd4714494c759d24d7d62f3b
Cillo98 <thomas.martin.cilloni@gmail.com>
2019-12-16 11:06:39 +0800

    Added ViewModel for all fragments

---
+++ app/src/main/java/com/armpits/nice/fragments/FragmentsViewModel.java
@@ -0,0 +1,38 @@
+package com.armpits.nice.fragments;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.armpits.nice.db.NiceDatabase;
+import com.armpits.nice.models.Deadline;
+import com.armpits.nice.models.Log;
+import com.armpits.nice.models.Module;
+
+import java.util.List;
+
+
+public class FragmentsViewModel extends ViewModel {
+    public LiveData<List<Module>> modules;
+    LiveData<List<Deadline>> deadlines;
+    LiveData<List<Log>> logs;
+
+    FragmentsViewModel() {
+        modules = NiceDatabase.getAllModules();
+        deadlines = NiceDatabase.getAllDeadlines();
+        logs = NiceDatabase.getAllLogs();
+    }
+
+//    private MediatorLiveData<List<Log>> logsMediator = new MediatorLiveData<>();
+//    private LiveData<List<Log>> logsLiveData;
+//    public Observable<List<Log>> logsList;
+//
+//    public FragmentsViewModel() {
+//        logsMediator.addSource(logsLiveData, logsList -> logsMediator.setValue(logsList));
+//    }
+//
+//    public void startLoading(LifecycleOwner owner) {
+//        logsMediator.observe(owner, logsList -> {
+//            logsList.set(logsList)
+//        });
+//    }
+}
